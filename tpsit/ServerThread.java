569fb70f55a7c50cb7130d8def527e23cad24f0d
Colausig <91591716+Colausig@users.noreply.github.com>
2023-01-31 22:16:31 +0100

    Add files via upload

---
+++ tpsit/ServerThread.java
@@ -0,0 +1,44 @@
+import java.net.*;
+import java.io.*;
+
+class ServerThread extends Thread {
+  ServerSocket server = null;
+  Socket client = null;
+  String stringaRicevuta = null;
+  String StringaModificata = null;
+  BufferedReader inDalClient;
+  DataOutputStream outVersoClient;
+
+  public ServerThread(Socket socket) {
+    this.client = socket;
+  }
+
+  public void run() {
+    try {
+      comunica();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void comunica() throws IOException {
+    inDalClient = new BufferedReader(new InputStreamReader(client.getInputStream()));
+    outVersoClient = new DataOutputStream(client.getOutputStream());
+
+    for (;;) {
+      stringaRicevuta = inDalClient.readLine();
+      if (stringaRicevuta == null || stringaRicevuta.equals("FINE")) {
+        outVersoClient.writeBytes(stringaRicevuta + " SERVER IN CHIUSURA " + "\n");
+        System.out.println("Echo su server in chiusura: " + stringaRicevuta);
+        break;
+      } else {
+        outVersoClient.writeBytes(stringaRicevuta + " ricevuta e ritrasmessa " + "\n");
+        System.out.println("Echo su server: " + stringaRicevuta);
+      }
+    }
+    outVersoClient.close();
+    inDalClient.close();
+    System.out.println("Chiusura socket " + client);
+    client.close();
+  }
+}
